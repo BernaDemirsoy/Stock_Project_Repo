c013b254f5ae8a118c7a777b6ed1e0da27e35414
Berna <93657008+BernaDemirsoy@users.noreply.github.com>
2023-05-12 17:08:59 +0300

    Entites,Repo,Context Added

---
+++ StockProject/StockProject.API/Controllers/CategoryController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StockProject.Entities.Entities;
+using StockProject.Service.Abstract;
+
+namespace StockProject.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly IGenericService<Category> service;
+
+        public CategoryController(IGenericService<Category> service)
+        {
+            this.service = service;
+        }
+        //GET: api/Category/GetAllCategories
+        [HttpGet]
+        public IActionResult GetAllCategories()
+        {
+            return Ok(service.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCategorybyId(int id)
+        {
+            var category=service.GetById(id);
+            if(category!=null)
+            return Ok(category);
+            else
+                return NotFound("Böyle bir kategori bulunamadı");
+        }
+        
+        [HttpPost]
+        public IActionResult CreateCategory([FromBody] Category category)
+        {
+            var result = service.Add(category);
+            if (result)
+            {
+                var addedcategory = service.GetById(category.Id);
+                return Ok(addedcategory);
+            }
+                
+            else
+                return NotFound("Kategori eklenemedi");
+        }
+        [HttpPut]
+        public IActionResult UpdateCategory([FromBody] Category category)
+        {
+            var updatedCategory = service.GetById(category.Id);
+            if (updatedCategory != null)
+            {
+                return Ok(service.Update(updatedCategory));
+            }
+            else
+                return NotFound("Kategori güncellenemedi");
+        }
+        [HttpDelete]
+        public IActionResult DeleteCategory(int id)
+        {
+            var updatedCategory = service.GetById(id);
+            if (updatedCategory != null)
+            {
+                return Ok(service.Remove(updatedCategory));
+            }
+            else
+                return NotFound("Kategori silinemedi");
+        }
+
+    }
+}
--- StockProject/StockProject.API/Program.cs
+++ StockProject/StockProject.API/Program.cs
@@ -1,3 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using StockProject.Repositories.Abstract;
+using StockProject.Repositories.Concrete;
+using StockProject.Repositories.Context;
+using StockProject.Service.Abstract;
+using StockProject.Service.Concrete;
+
 namespace StockProject.API
 {
     public class Program
@@ -12,6 +19,14 @@ namespace StockProject.API
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<StockProjectContext>(options => {
+                options.UseSqlServer("Server=LAPTOP-5JQ1KEF4;Database=DBStock_Project;Trusted_Connection=True;");
+                
+             });
+            builder.Services.AddTransient(typeof(IGenericService<>), typeof(GenericService<>));
+
+            builder.Services.AddTransient(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 
             var app = builder.Build();
 
