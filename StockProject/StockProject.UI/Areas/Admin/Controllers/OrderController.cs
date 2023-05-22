7acc430350e45bc8182fa702708e858460effacc
Berna <93657008+BernaDemirsoy@users.noreply.github.com>
2023-05-22 17:01:27 +0300

    cmt2

--- StockProject/StockProject.UI/Areas/Admin/Controllers/OrderController.cs
+++ StockProject/StockProject.UI/Areas/Admin/Controllers/OrderController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using StockProject.Entities.Entities;
+using StockProject.UI.Areas.Admin.Models.DTOs;
 using System.Data;
 
 namespace StockProject.UI.Areas.Admin.Controllers
@@ -26,6 +27,62 @@ namespace StockProject.UI.Areas.Admin.Controllers
             }
             return View(orders);
 
+        }
+        public static Order _order;
+        public static List<OrderDetail> orderDetails;
+
+        public async Task<IActionResult> DetailOrder(int id)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                using (var answ = await httpClient.GetAsync($"{baseURL}/api/Order/GetOrderById/{id}"))
+                {
+                    string apiResult = await answ.Content.ReadAsStringAsync();
+                    _order = JsonConvert.DeserializeObject<List<Order>>(apiResult)[0];
+                }
+                using (var answ = await httpClient.GetAsync($"{baseURL}/api/Order/GetOrderDetails/{id}"))
+                {
+                    string apiResult = await answ.Content.ReadAsStringAsync();
+                    orderDetails = JsonConvert.DeserializeObject<List<OrderDetail>>(apiResult);
+                }
+
+            }
+
+            OrderDetailDTO orderDetailDTO = new OrderDetailDTO();
+            orderDetailDTO.Order = _order;
+            orderDetailDTO.OrderDetails = orderDetails;
+
+            return View(orderDetailDTO);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ConfirmedOrder(int id)
+        {
+            using (var httpClient = new HttpClient())
+            {
+               
+                using (var answ = await httpClient.GetAsync($"{baseURL}/api/Order/ConfirmedToOrder/{id}"))
+                {
+                   
+                   
+                }
+               
+
+            }
+            return RedirectToAction("Index");
+        }
+        public async Task<IActionResult> CancelledOrder(int id)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                
+                using (var answ = await httpClient.GetAsync($"{baseURL}/api/Order/CanceledToOrder/{id}"))
+                {
+                   
+                }
+
+            }
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ StockProject/StockProject.UI/Areas/Admin/Models/DTOs/OrderDetailDTO.cs
@@ -0,0 +1,10 @@
+﻿using StockProject.Entities.Entities;
+
+namespace StockProject.UI.Areas.Admin.Models.DTOs
+{
+    public class OrderDetailDTO
+    {
+        public List<OrderDetail> OrderDetails { get; set; }
+        public Order Order { get; set; }
+    }
+}
